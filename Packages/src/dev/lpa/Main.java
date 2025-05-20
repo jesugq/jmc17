f7393e2184a4fc3e559d8349a8ccca260b1b4a05
jesugq <jesugq@outlook.com>
2025-05-20 09:31:08 -0600

    Add Packages

---
+++ Packages/src/dev/lpa/Main.java
@@ -0,0 +1,15 @@
+package dev.lpa;
+
+import com.abc.first.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        com.abc.first.Item firstItem = new com.abc.first.Item("Burger");
+//        System.out.println(firstItem);
+
+        Item firstItem = new Item("Burger");
+        System.out.println(firstItem);
+    }
+}
